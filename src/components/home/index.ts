686a19d546b43406a77d3fd86715079c291ee026
Ramerlin <ramerlin.castillo@botcity.com.do>
2025-09-07 21:42:32 -0400

    Fix import path for infoData in contact component and add home component exports

---
+++ src/components/home/index.ts
@@ -0,0 +1,4 @@
+export { default as Hero } from './hero.astro';
+export { default as Gallery } from './galleryShop.astro';
+export { default as GalleryCategories } from './galleryCategories.astro';
+export { default as Cta } from './cta.astro';
