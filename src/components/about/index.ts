5fa51e747ef6860d1715a0d65c8c25d47b658585
Ramerlin <ramerlin.castillo@botcity.com.do>
2025-09-07 21:42:23 -0400

    Add About and Contact component exports for better modularity

---
+++ src/components/about/index.ts
@@ -0,0 +1,4 @@
+export { default as AboutHero } from './hero.astro';
+export { default as AboutMission } from './mission.astro';
+export { default as AboutValues } from './values.astro';
+export { default as AboutTeam } from './team.astro';
---
+++ src/components/contact/index.ts
@@ -0,0 +1,4 @@
+export { default as ContactHero } from './hero.astro';
+export { default as ContactInfo } from './info.astro';
+export { default as ContactForm } from './form.astro';
+export { default as ContactMap } from './map.astro';
